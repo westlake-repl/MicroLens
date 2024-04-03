3d3c3a147d1069c7483e042189ed7dbccc9c83ff
yxni98 <60702500+yxni98@users.noreply.github.com>
2024-04-04 01:08:46 +0800

    Update feature_extraction.py

--- Data Processing/feature_extraction.py
+++ Data Processing/feature_extraction.py
@@ -5,21 +5,18 @@ import torch.nn as nn
 
 device = torch.device('cuda:4' if torch.cuda.is_available() else "cpu") 
 
-# save each video as a class with byte data, which can be decoded from lmdb database.
-# do not remove the following class
 class LMDB_VIDEO:
     def __init__(self, video):
         self.video = video.tobytes()
 
 frame_no = 5
-video_lmdb_path = '../100wu_fi1_fn5_frames.lmdb'
+video_lmdb_path = 'D:/video_frames_100k.lmdb'
 
-video_number = 91402 # to input
+video_number = 19738 # to input
 
 avg_pool = nn.AdaptiveAvgPool2d((1, 768))
 configuration = VideoMAEConfig(num_frames=frame_no)
-# download link for videomae: https://huggingface.co/MCG-NJU/videomae-base/tree/main
-video_model = VideoMAEModel.from_pretrained('/home/public/data/pretrained_models/videomae_base', config = configuration).to(device)
+video_model = VideoMAEModel.from_pretrained('E:/MM_DATASET/DATA_GENERATION/videomae_base', config = configuration).to(device)
 
 env = lmdb.open(video_lmdb_path, subdir=os.path.isdir(video_lmdb_path),
                         readonly=True, lock=False, readahead=False, meminit=False)
@@ -28,7 +25,7 @@ all_scoring = torch.tensor([]).to(torch.float32).to('cpu')
 with torch.no_grad():
     with env.begin() as txn:
         for i in range(1, video_number+1, 100):
-            if i != (video_number%100+1):
+            if i != (video_number-video_number%100+1):
                 all_videos = np.zeros((100, 5, 3, 224, 224))
                 max_step = 100
             else:
@@ -42,9 +39,7 @@ with torch.no_grad():
                 k += 1
             all_videos = torch.from_numpy(all_videos).to(torch.float32).to(device)
             item_scoring = video_model(all_videos).last_hidden_state
-            # torch.Size([112, 392, 768])
             item_scoring = avg_pool(item_scoring)
-            # torch.Size([112, 1, 768])
             item_scoring = item_scoring.squeeze(1) # torch.Size([112, 512])
             item_scoring = item_scoring.to('cpu')
             all_scoring = torch.cat([all_scoring, item_scoring], 0)
